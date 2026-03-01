082091660e333495220e33055539827d1051f793
adil-bennani-corps <adil-bennani@hotmail.com>
2026-03-01 00:49:19 +0000

    Add remaining changes and clean up deletions

--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import { serviceSchema } from "./service";
-import { projectSchema } from "./project";
-import { faqSchema } from "./faq";
-
-export const schemaTypes = [serviceSchema, projectSchema, faqSchema];
